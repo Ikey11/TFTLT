9973508b696c1c72f10f2892747a53f15c2597fc
Ikey11 <maysgaming00@gmail.com>
2024-12-07 17:11:17 -0800

    new body font

--- quartz.config.ts
+++ quartz.config.ts
@@ -25,7 +25,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Lugrasimo",
-        body: "Fondamento",
+        body: "Amarante",
         code: "IBM Plex Mono",
       },
       colors: {
